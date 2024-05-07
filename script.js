5b79bfad2f938efdcbaf38c573eaa265f82328b1
Anastasios3 <117446378+Anastasios3@users.noreply.github.com>
2024-05-07 18:13:08 +0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,63 @@
+'use strict'; // can help with debugging and more. as i can understand use it always! lots of words are reserved for future use. like interface & private (you cannot use the name function !)
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive");
+
+
+
+function logger() {
+    console.log("My name is Jonas");
+}
+// calling - running - invoking the function
+logger();
+
+function fruitProcessor(apples, oranges) {
+    //console.log(apples, oranges);
+    const juice = `Juice with ${apples} apple(s) 🍎 & ${oranges} orange(s) 🍊.`;
+    return juice;
+}
+
+const mixedFruitJuice = fruitProcessor(2, 9);
+console.log(mixedFruitJuice);
+
+const appleOrangeJuice = fruitProcessor(3, 2);
+console.log(appleOrangeJuice); // D.R.Y. - Don't Repeat Yourself - That's why functions are great!
+
+//function decleration      -- you can call them defore declaring them!! That's cool!!
+console.log(calcAge1(1994));
+
+function calcAge1(birthYear) {
+    return 2024 - birthYear;
+}
+
+
+//function expression       
+const calcAge2 = function (birthYear) { //Aninymus function
+    return 2025 - birthYear;
+}
+
+console.log(calcAge2(1994));
+
+const calcAge2 = function (birthYear) { //Aninymus function
+    return 2025 - birthYear;
+}
+
+// Arrow function
+const calcAge3 = birthYear => 2037 - birthYear;  //one line  function
+const age3 = calcAge3(1994);
+console.log(age3);
+
+const yearsUntilRetirment = (birthYear, firstName) => {
+    const age = 2024 - birthYear;
+    const retirement = 65 - age;
+    //return retirement;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirment(1994, "Anastasios"));
+console.log(yearsUntilRetirment(1973, "Afroditi"));
+*/
+
